8bb3c9c67c508cee30224cee14dd6e9344356cfa
FlavaClover <zaur_from_flava@icloud.com>
2022-02-18 01:34:47 +0300

    Добавлен метод для более удобной генерации случайных INSERT запросов

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from worker import Worker, WorkerManager
 from department import Department, DepartmentManager
+from manager import Manager
 from datetime import datetime
 
 
+DB_NAME = 'data_base'
+
+
 if __name__ == "__main__":
-    pass
+    d_manager = DepartmentManager(DB_NAME)
+
+    depts = d_manager.generate_random_inserts(
+            'departments', count=1000,
+            renames_col={'id_dept': 'id', 'name': 'name_dept'})
+    for i in depts:
+        print(i)
+
+
--- manager.py
+++ manager.py
@@ -7,7 +7,8 @@ class Manager:
         if not self.db_name.endswith('.db'):
             self.db_name += '.db'
 
-    def generate_inserts(self, objs: list, table_name: str, ignore=[]):
+    def __generate_inserts(self, objs: list, table_name: str,
+                           ignore=[], renames_col={}):
         inserts = list()
         for obj in objs:
             sql = f'INSERT INTO {table_name} '
@@ -18,13 +19,25 @@ class Manager:
 
             for attr in attributes:
                 if attributes[attr] and attr not in ignore:
-                    cols += attr + ', '
                     values += '\'' + str(attributes[attr]) + '\', '
+                    if attr in renames_col:
+                        attr = renames_col[attr]
+                    cols += attr + ', '
             sql += cols[:len(cols) - 2] + ') VALUES '
             sql += values[:len(values) - 2] + ')'
 
             inserts.append(sql)
         return inserts
+
+    def random(self, count=1):
+        return list()
+
+    def generate_random_inserts(self, table_name: str, count=1,
+                                ignore=[], renames_col={}):
+        objs = self.random(count=count)
+        return self.__generate_inserts(objs, table_name,
+                                       ignore=ignore,
+                                       renames_col=renames_col)
 
     def check_db(self):
         try:
